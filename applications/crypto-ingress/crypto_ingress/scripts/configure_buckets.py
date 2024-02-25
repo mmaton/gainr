4c029f4436d05c31b314f81ed7436bc9b9d3c1dd
Max Maton <me@mmaton.com>
2024-02-25 13:59:29 +0000

    Don't create downsample tasks, we're going to downsample in-app

--- applications/crypto-ingress/crypto_ingress/scripts/configure_buckets.py
+++ applications/crypto-ingress/crypto_ingress/scripts/configure_buckets.py
@@ -27,47 +27,7 @@ class Action(Enum):
         return self.value
 
 
-def create_downsample_query(from_timeframe: str, to_timeframe: str, offset: int = 0):
-    # https://community.influxdata.com/t/flux-multiple-aggregates/10221/8
-    flux = dedent(f'''
-        import "date"
-        option task = {{name: "{env}_ohlc_downsample_{to_timeframe}", every: {to_timeframe}, offset: {offset}s}}
-        
-        data = () =>  from(bucket: "{env}_ohlc_{from_timeframe}")
-         |> range(
-                start: date.sub(d: {to_timeframe}, from: date.truncate(t: now(), unit: {to_timeframe})),
-                stop: date.truncate(t: now(), unit: {to_timeframe})
-            )
-         |> sort(columns: ["_time"])
-        
-        aggregate = (tables=<-, filterFn, agg, name) =>
-                    tables
-                        |> filter(fn: filterFn)
-                        |> aggregateWindow(every: {to_timeframe}, fn: agg)
-                        |> set(key: "_field", value: name)
-                        |> duplicate(column: "_start", as: "_time")
-        
-        union(
-            tables: [
-                data() |> aggregate(filterFn: (r) => r._field == "open", agg: first, name: "open"),
-                data() |> aggregate(filterFn: (r) => r._field == "high", agg: max, name: "high"),
-                data() |> aggregate(filterFn: (r) => r._field == "low", agg: min, name: "low"),
-                data() |> aggregate(filterFn: (r) => r._field == "close", agg: last, name: "close"),
-                data() |> aggregate(filterFn: (r) => r._field == "trades", agg: sum, name: "trades"),
-                data() |> aggregate(filterFn: (r) => r._field == "volume", agg: sum, name: "volume"),
-            ],
-        )
-        |> to(bucket: "{env}_ohlc_{to_timeframe}", org: "influxdata")
-    ''')
-    return TaskCreateRequest(
-        org=config.INFLUXDB_ORG,
-        flux=flux,
-    )
-
-
 def install():
-    last_tf = None
-    offset_count = 1
     for tf in TIMEFRAMES:
         try:
             influxdb_client.buckets_api().create_bucket(bucket_name=f"{env}_ohlc_{tf}")
@@ -79,26 +39,8 @@ def install():
             else:
                 raise e
 
-        if last_tf:
-            find_task = influxdb_client.tasks_api().find_tasks(name=f"{env}_ohlc_downsample_{tf}")
-            offset = DOWNSAMPLE_OFFSET_MULTIPLIER * offset_count
-            query = create_downsample_query(from_timeframe=last_tf, to_timeframe=tf, offset=offset)
-            if not find_task:
-                # Create task
-                influxdb_client.tasks_api().create_task(task_create_request=query)
-                config.logger.info(f"Created downsample task {env}_ohlc_downsample_{tf}")
-            else:
-                # Update
-                find_task[0].flux = query.flux
-                influxdb_client.tasks_api().update_task(task=find_task[0])
-                config.logger.info(f"Updated downsample task {env}_ohlc_downsample_{tf}")
-            offset_count += 1
-
-        last_tf = tf
-
 
 def uninstall():
-    last_tf = None
     for tf in TIMEFRAMES:
         find_bucket = influxdb_client.buckets_api().find_bucket_by_name(bucket_name=f"{env}_ohlc_{tf}")
         if find_bucket:
@@ -106,15 +48,6 @@ def uninstall():
             config.logger.info(f"Deleted bucket {env}_ohlc_{tf}")
         else:
             config.logger.info(f"Not deleting bucket {env}_ohlc_{tf}, it doesn't exist!")
-
-        if last_tf:
-            find_task = influxdb_client.tasks_api().find_tasks(name=f"{env}_ohlc_downsample_{tf}")
-            if find_task:
-                # Delete
-                influxdb_client.tasks_api().delete_task(task_id=find_task[0].id)
-                config.logger.info(f"Deleted downsample task {env}_ohlc_downsample_{tf}")
-
-        last_tf = tf
 
 
 if __name__ == "__main__":
