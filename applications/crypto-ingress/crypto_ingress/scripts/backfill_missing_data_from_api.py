685228ffee12257a81929fdc9f593a4ffd363b69
Max Maton <me@mmaton.com>
2024-02-27 13:11:09 +0000

    Implement backfill from API script

--- applications/crypto-ingress/crypto_ingress/scripts/backfill_missing_data_from_api.py
+++ applications/crypto-ingress/crypto_ingress/scripts/backfill_missing_data_from_api.py
@@ -10,3 +10,43 @@ But we can still work on higher timeframes.
 
 720 hours = 30 days
 """
+import asyncio
+import tenacity
+
+from crypto_ingress.config import OHLC_INTERVALS, SYMBOLS_TO_WATCH, logger, ENVIRONMENT
+from crypto_ingress.influxdb import InfluxClient
+from crypto_ingress.influxdb_point_formats import format_influxdb_ohlc, ohlc_to_dict
+from crypto_ingress.kraken import KrakenMarket
+
+
+async def main():
+    market_client = KrakenMarket()
+
+    @tenacity.retry(
+        wait=tenacity.wait_random_exponential(multiplier=1, max=60),
+        stop=tenacity.stop_after_delay(60),
+        reraise=True,
+    )
+    def get_candles(symbol: str, interval: int):
+        try:
+            candles = market_client.get_ohlc(pair=symbol, interval=interval, since=0)
+        except Exception as e:
+            logger.error(f"Got error when fetching 720 candles for {symbol} {interval}: {e}")
+            raise e
+        return candles[symbol]
+
+    async with InfluxClient() as influx_client:
+        write_api = influx_client.write_api()
+
+        for symbol in SYMBOLS_TO_WATCH:
+            for interval, interval_friendly in OHLC_INTERVALS:
+                logger.info(f"Getting candles for {symbol} {interval_friendly}....")
+                candles = await ohlc_to_dict(get_candles(symbol, interval), symbol, interval)
+                points = await format_influxdb_ohlc(candles)
+                logger.info(f"Writing candles {symbol} {interval_friendly}....")
+                await write_api.write(bucket=f"{ENVIRONMENT}_ohlc_{interval_friendly}", record=points)
+                logger.info(f"Indexed {len(candles)} for {symbol} {interval_friendly}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
